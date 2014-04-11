7a8acdf0658e496a0273c79e89264dde73b35681
Fábio Batista <fabio@elementarsistemas.com.br>
2014-04-11 02:24:08 -0300

    Using express.js and jade

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ var url = require('url');
 var querystring = require('querystring');
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+var express = require('express')
+var stylus = require('stylus')
+var nib = require('nib')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str).set('filename', path).use(nib());
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
   fetch_fql('select object_id from photo where owner = 129079313911235 order by created desc limit 3', function(err, r) {
     if (err) {
       res.writeHead(500, { 'Content-Type': 'text/html' });
@@ -23,11 +35,12 @@ var server = http.createServer(function(req, res) {
         res.writeHead(500, { 'Content-Type': 'text/html' });
         return res.end(err.message);
       }
-      res.end(render(parse(r)));
+      // res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.render('index', { r: parse(r) });
     });
   });
 });
-server.listen(3000);
+app.listen(3000)
 
 function fetch_fql(fql, callback) {
   http.get({ host: 'graph.facebook.com', path: '/fql?' + querystring.stringify({q: fql}) }, function(res) {
@@ -65,18 +78,4 @@ function parse(json) {
     }
   });
   return r;
-}
-
-function render(r) {
-  var s = '';
-  s += '<table style="width: 100%; table-layout: fixed"><thead><tr><th>Chips</th><th>OJs</th><th>Coins</th></tr></thead><tbody><tr valign="top">';
-  s += '<td>';
-  s += r.chips.map(function(uri) { return '<a href="' + uri.s + '">' + uri.parsed.query.reward_key + '</a><br>'; }).join('');
-  s += '</td><td>';
-  s += r.ojs.map(function(uri) { return '<a href="' + uri.s + '">' + uri.parsed.query.reward_key + '</a><br>'; }).join('');
-  s += '</td><td>';
-  s += r.coins.map(function(uri) { return '<a href="' + uri.s + '">' + uri.parsed.query.reward_key + '</a><br>'; }).join('');
-  s += '</td></table>';
-  // s += '<pre>' + JSON.stringify(r, null, '  ') + '</pre>';
-  return s;
 }

4c1886cedb21f5682edbfce65d6a590ad2fbe0ce
Fábio D. Batista <fabio.david.batista@gmail.com>
2014-04-21 04:18:31 -0400

    Refactored to run on client side, and added a better layout

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var url = require('url');
 var querystring = require('querystring');
 var http = require('http');
 
-var express = require('express')
-var stylus = require('stylus')
-var nib = require('nib')
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
 
-var app = express()
+var app = express();
 function compile(str, path) {
   return stylus(str).set('filename', path).use(nib());
 }
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/json', function(req, res) {
   fetch_fql('select object_id from photo where owner = 129079313911235 order by created desc limit 10', function(err, r) {
     if (err) {
       res.writeHead(500, { 'Content-Type': 'text/html' });
@@ -31,13 +31,15 @@ app.get('/', function(req, res) {
     var photo_ids = r.data.map(function(d) { return d.object_id; }).join(', ');
     
     fetch_fql('select id, text, time from comment where object_id in (' + photo_ids + ') order by time desc', function(err, r) {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
-        return res.end(err.message);
-      }
-      res.render('index', { r: parse(r) });
+      if (err)
+        return res.json(500, { error: err.message });
+      res.json(parse(r));
     });
   });
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
 });
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
 
@@ -52,29 +54,27 @@ function fetch_fql(fql, callback) {
         callback(null, JSON.parse(data));
       else
         callback(new Error(data));
-    })
+    });
   });
 }
 
 function parse(json) {
   var r = { chips: [], ojs: [], coins: [], json: json };
   json.data.forEach(function(comment) {
-    var m;
-    if (m = /http\S+/.exec(comment.text)) {
-      m.forEach(function(uri) {
-        var parsed = url.parse(uri, true);
-        
-        // reward_type => 0: oj, 1: chips, 4: coins
-        var slot = null;
-        switch (Number(parsed.query.reward_type)) {
-          case 0: slot = 'ojs'; break;
-          case 1: slot = 'chips'; break;
-          case 4: slot = 'coins'; break;
-        }
-        if (slot && !r[slot].some(function(el) { return el.s === uri; }))
-          r[slot].push({s: uri, parsed: parsed});
-      });
-    }
+    var m = /http\S+/.exec(comment.text);
+    (m || []).forEach(function(uri) {
+      var parsed = url.parse(uri, true);
+      
+      // reward_type => 0: oj, 1: chips, 4: coins
+      var slot = null;
+      switch (Number(parsed.query.reward_type)) {
+        case 0: slot = 'ojs'; break;
+        case 1: slot = 'chips'; break;
+        case 4: slot = 'coins'; break;
+      }
+      if (slot && !r[slot].some(function(el) { return el.s === uri; }))
+        r[slot].push({s: uri, parsed: parsed});
+    });
   });
   return r;
 }
